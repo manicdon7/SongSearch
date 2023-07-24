58d2c59c4f0dee2d79604795e691b02dccb271be
Mani kandan <manicdon7@example.com>
2023-07-24 20:49:39 +0530

    Reactapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
+import Musiccard from "./MusicCard";
+import SearchIcon from "./search.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const API_URL = "Your_ApiKey_Here";
+
+const App = () => {
+  const [searchterm, setsearchterm] = useState("");
+  const [songs,setsongs] = useState([]);
+  
+  useEffect(()=>{
+    searchsongs("seach your fav here...");
+  },[]);
+
+  const searchsongs = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    
+    setsongs(data.search);
+  };
+  return(
+    <div className='app'>
+      <h1>SongsIland</h1>
+      <div className='search'>
+        <input value={searchterm} onChange={(e)=> setsearchterm(e.target.value)} placeholder='Seach songs here'/>
+        <img src={SearchIcon} alt="search" onClick={() => searchsongs(searchterm)} />
+      </div>
+
+      {songs.length > 0 ? (
+        <div className="container">
+          {songs.map((song) => (
+            <Musiccard song={song} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No songs found</h2>
+        </div>
+      )}
     </div>
   );
 }
